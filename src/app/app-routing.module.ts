dd86e19e932317d1b9b9754da9ff8c5d79ab1d09
ritesh kumar <ritesh.kumar@digitalindiatalent.com>
2021-01-05 11:26:25 +0530

    sign in form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './component/about-us/about-us.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login-component', component: LoginComponent },
   { path: 'signup-component', component: SignupComponent },
   { path: 'about-us-component', component: AboutUsComponent },
+  { path: 'contact-us-component', component: ContactUsComponent }
 ];
 
 @NgModule({
